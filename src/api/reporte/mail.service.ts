2dea34cb4cabf942297a65fb96427e384fe480fc
muimui69 <moises_c2015@outlook.es>
2025-02-08 07:22:32 -0400

    completo

--- src/api/reporte/mail.service.ts
+++ src/api/reporte/mail.service.ts
@@ -4,24 +4,19 @@ import * as nodemailer from 'nodemailer';
 @Injectable()
 export class MailService {
     private transporter = nodemailer.createTransport({
-        host: 'tecnoweb.edu', // Cambia esto si usas Gmail o Outlook
+        host: 'smtp.gmail.com', // Cambia esto si usas Gmail o Outlook
         port: 587,
         secure: false,
         auth: {
-            user: 'ana@tecnoweb.edu',
-            pass: '1234'
+            user: 'junior.zf.99@gmail.com',
+            pass: 'exis igvj uegr tmjc'
         },
-        logger: true,
-        debug: true,
-        tls: {
-            rejectUnauthorized: false // Si estás usando un certificado autofirmado
-        }
     });
 
     async enviarCorreoConAdjunto(destinatarios: string[], subject: string, body: string, pdfBuffer: Buffer, fileName: string) {
         try {
             const info = await this.transporter.sendMail({
-                from: '"Tecnoweb" <ana@tecnoweb.edu>',
+                from: '"Tkm jeans" <sopote@tecnoweb.edu>',
                 to: destinatarios.join(', '),
                 subject,
                 text: body,

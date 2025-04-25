ef3e65966b82598c0410ccb4eab6198e7a4123fd
Willonlyonexd <houwenvt@gmail.com>
2025-04-25 01:02:50 -0400

    integracion modelo segmentacion v1

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsuarioModule } from './api/usuario/usuario.module';
-import { SegmentationModule } from './api/segmentation/segmentation.module';
+import { SegmentationModule } from './api/ml-segmentation/segmentation.module';
 
 import { ProductoModule } from './api/producto/producto.module';
 
